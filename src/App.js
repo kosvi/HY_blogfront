70fda0768f06163e4c92705b8c4f8c47c5244aee
Ville Koskela <vkoskela@gmail.com>
2021-10-05 20:44:13 +0300

    5.1 done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
 import React, { useState, useEffect } from 'react'
-import Blog from './components/Blog'
+import Blogs from './components/Blogs'
+import Login from './components/Login'
+import ErrorMessage from './components/ErrorMessage'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (error) {
+      setErrorMessage('learn to type!')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        {
+          errorMessage !== null && <ErrorMessage message={errorMessage} />
+        }
+        <h2>log in to application</h2>
+        <Login
+          user={username}
+          pwd={password}
+          setUser={setUsername}
+          setPwd={setPassword}
+          handleLogin={handleLogin}
+        />
+      </div>
+    )
+  }
+  if (user !== null) {
+    return (
+      <div>
+        {
+          errorMessage !== null && <ErrorMessage message={errorMessage} />
+        }
+        <h2>blogs</h2>
+        logged in as {user.name} <br /><br />
+        <Blogs blogs={blogs} />
+      </div>
+    )
+  }
 }
 
 export default App
